bd4bd7c88978e27a1e418f75341cece0d8465a61
Danish Al-Muhaimin Mustaffa <102360576+danisham123@users.noreply.github.com>
2022-07-07 19:35:07 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+import java.sql.*; 
+import java.util.Scanner;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		// Create a view
+		Student_UI view = new Student_UI();
+		
+		
+		System.out.print("1. Input Marks \n2. Check Student's Mark \nSelect: ");
+		int option = input.nextInt();
+		
+		//display different view base on the option
+		view.displayMenu(option);
+		
+	
+	}
+}
